e2f0d6b355019e62fa462b854549b05f09a6ecf2
Francisca <francisca.paredes.pr@gmail.com>
2025-01-29 09:42:57 -0300

    cuenta unica

--- Cap1/HigherOrLowerProcedural.py
+++ Cap1/HigherOrLowerProcedural.py
@@ -1,80 +1,78 @@
-# HigherOrLower
-
 import random
 
-# Card constants
-SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')
-RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')
+# Constantes de cartas
+SUIT_TUPLE = ('Spades', 'Hearts', 'Clubs', 'Diamonds')  # Palos: Espadas, Corazones, Tréboles, Diamantes
+RANK_TUPLE = ('Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King')  # Rangos de cartas
 
-NCARDS = 8
+NCARDS = 8  # Número de cartas a jugar por partida
 
-# Pass in deck and this function returns a random card from the check
+# Recibe un mazo y devuelve una carta aleatoria del mazo
 def getCard(deckListIn):
-    thisCard = deckListIn.pop()    # pop one off the top of the deck and return
+    thisCard = deckListIn.pop()  # Extrae una carta de la parte superior del mazo y la devuelve
     return thisCard
 
-# Pass in a deck and this function returns a shuffled copy of the deck
+# Recibe un mazo y devuelve una copia barajada del mismo
 def shuffle(deckListIn):
-    deckListOut = deckListIn.copy()    # make a copy of the starting deck
-    random.shuffle(deckListOut)
+    deckListOut = deckListIn.copy()  # Hace una copia del mazo original
+    random.shuffle(deckListOut)  # Baraja las cartas
     return deckListOut
 
-# Main code
-print('Welcome to Higher or Lower.')
-print('You hace to choose whether the next card to be shown will be higher or lower the current card.')
-print('Getting it right adds 20 points; get it wrong and you lose 15 points.')
-print('You hace 50 point to start.')
+# Código principal
+print('Bienvenido a Higher or Lower.')
+print('Debes elegir si la siguiente carta que se mostrará será mayor o menor que la carta actual.')
+print('Si aciertas, ganas 20 puntos; si te equivocas, pierdes 15 puntos.')
+print('Empiezas con 50 puntos.')
 print()
 
 startingDeckList = []
 for suit in SUIT_TUPLE:
     for thisValue, rank in enumerate(RANK_TUPLE):
-        cardDict = {'rank': rank, 'suit': suit,
-                    'value': thisValue + 1}
+        cardDict = {'rank': rank, 'suit': suit, 'value': thisValue + 1}  # Se asigna un valor numérico a cada carta
         startingDeckList.append(cardDict)
 
-        score = 50
+score = 50  # Puntaje inicial
 
-        while True:    # Paly multiple games
-            print()
-            gameDeckList = shuffle(startingDeckList)
-            currentCardDict = getCard(gameDeckList)
-            currentCardRank = currentCardDict['rank']
-            currentCardValue = currentCardDict['value']
-            currentCardSuit = currentCardDict['suit']
-            print('Starting card is:', currentCardRank + ' of ' + currentCardSuit)
-            print()
+while True:  # Permite jugar múltiples partidas
+    print()
+    gameDeckList = shuffle(startingDeckList)  # Baraja el mazo
+    currentCardDict = getCard(gameDeckList)  # Extrae la primera carta
+    currentCardRank = currentCardDict['rank']
+    currentCardValue = currentCardDict['value']
+    currentCardSuit = currentCardDict['suit']
+    print('La carta inicial es:', currentCardRank + ' de ' + currentCardSuit)
+    print()
 
-            for cardNumber in range(0, NCARDS):    # Play one game of this many cards
-                answer = input('Will the next card b higher or lower than the ' + currentCardRank + ' of ' + currentCardSuit + '? (enter h or 1):')
-                answer = answer.casefold()    # force lowercase
-                nextCardDict = getCard(gameDeckList)
-                nextCardRank = nextCardDict['rank']
-                nextCardSuit = nextCardDict['suit']
-                nextCardValue = nextCardDict['value']
-                print('Next card is:' + nextCardRank + ' of ' + nextCardSuit)
+    for cardNumber in range(0, NCARDS):  # Juega una partida con este número de cartas
+        answer = input('¿La siguiente carta será mayor o menor que ' + currentCardRank + ' de ' + currentCardSuit + '? (ingresa "h" o "l"): ')
+        answer = answer.casefold()  # Convierte la respuesta a minúsculas
+        nextCardDict = getCard(gameDeckList)  # Extrae la siguiente carta
+        nextCardRank = nextCardDict['rank']
+        nextCardSuit = nextCardDict['suit']
+        nextCardValue = nextCardDict['value']
+        print('La siguiente carta es: ' + nextCardRank + ' de ' + nextCardSuit)
 
-                if answer == 'h':
-                    if nextCardValue > currentCardValue:
-                        print('You got it rigth, it was higher')
-                        score = score + 20
-                    else:
-                        print('Sorry, it was not higher')
-                        score = score - 15
-                elif answer == '1':
-                    if nextCardValue < currentCardValue:
-                        score = score + 20
-                        print('You got it right, it was lower')
-                    else:
-                        score = score - 15
-                        print('Sorry, it was not lower')
-                print('Your score is: ', score)
-                print()
-                currentCardRank = nextCardRank
-                currentCardValue = nextCardValue    # don`t need current suit
+        if answer == 'h':  # Si el jugador eligió "mayor"
+            if nextCardValue > currentCardValue:
+                print('¡Correcto, era mayor!')
+                score = score + 20
+            else:
+                print('Lo siento, no era mayor.')
+                score = score - 15
+        elif answer == 'l':  # Si el jugador eligió "menor"
+            if nextCardValue < currentCardValue:
+                score = score + 20
+                print('¡Correcto, era menor!')
+            else:
+                score = score - 15
+                print('Lo siento, no era menor.')
+        
+        print('Tu puntaje es:', score)
+        print()
+        currentCardRank = nextCardRank
+        currentCardValue = nextCardValue  # No es necesario actualizar el palo actual
 
-            goAgain = input('To play again, press ENTER, or "q" to quit: ')
-            if goAgain == 'q':
-                break
+    goAgain = input('Para jugar de nuevo, presiona ENTER, o "q" para salir: ')
+    if goAgain == 'q':
+        break
 
-            print('OK bye')
+print('¡Hasta luego!')
