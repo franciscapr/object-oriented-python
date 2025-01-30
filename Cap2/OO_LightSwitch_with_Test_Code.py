f12d995ff899978731d8dd8560cbf6a3a6d09fcf
Francisca <francisca.paredes.pr@gmail.com>
2025-01-30 09:55:13 -0300

    cap2

---
+++ Cap2/OO_LightSwitch_with_Test_Code.py
@@ -0,0 +1,28 @@
+# OOR_LightSwitch
+
+class LightSwitch():
+    def __init__(self):
+        self.switchIsOn = False
+
+    def turnOn(self):
+        # turn the switch on
+        self.switchIsOn = True
+
+    def turnOff(self):
+        # turn the switch off
+        self.switchIsOn = False
+
+    def show(self):    # added for testing
+        print(self.switchIsOn)
+
+# Main code
+oLightSwitch = LightSwitch()    # create a LightSwitch object
+
+# calls to methods
+oLightSwitch.show()
+oLightSwitch.turnOn()
+oLightSwitch.show()
+oLightSwitch.turnOff()
+oLightSwitch.show()
+oLightSwitch.turnOn()
+oLightSwitch.show()
