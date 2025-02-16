d35ebbcab9b0698e61ec47da989befee57d9fc60
Francisca <francisca.paredes.pr@gmail.com>
2025-02-16 11:37:36 -0300

    main

--- Cap6/PygameDemo7_SimpleButton/Main_SImpleButton3Buttons.py
+++ Cap6/PygameDemo7_SimpleButton/Main_SImpleButton3Buttons.py
@@ -1,26 +1,26 @@
-# pygame demo 7 3-Button test
- 
-# 1 - Import packages
+# pygame demo 7 - Prueba de 3 botones
+
+# 1 - Importar paquetes
 import pygame
 from pygame.locals import *
 from SimpleButton import *
 import sys
 
-# Define constantes
+# Definir constantes
 GRAY = (200, 200, 200)
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 100
 FRAMES_PER_SECOND = 30
 
-# 2 - Initialize the word
+# 2 - Inicializar el mundo
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 clock = pygame.time.Clock()
 
-# 4 - Load assets: image(s), sounds, etc.
+# 4 - Cargar recursos: imágenes, sonidos, etc.
 
-# 5 - Initialize variables
-# Create instances of SimpleButton
+# 5 - Inicializar variables
+# Crear instancias de SimpleButton
 oButtonA = SimpleButton(window, (25, 30),
                         'Cap6/PygameDemo7_SimpleButton/images/buttonAUp.png',
                         'Cap6/PygameDemo7_SimpleButton/images/buttonADown.png')
@@ -31,36 +31,35 @@ oButtonC = SimpleButton(window, (275, 30),
                         'Cap6/PygameDemo7_SimpleButton/images/buttonCUp.png',
                         'Cap6/PygameDemo7_SimpleButton/images/buttonCDown.png')
 
-# 6 - Loop forever
+# 6 - Bucle infinito
 while True:
 
-    # 7 - Check for and handle events
+    # 7 - Comprobar y manejar eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-            # Pass the event to each button, see if one has been clicked
+        # Pasar el evento a cada botón para ver si se ha hecho clic en alguno
         if oButtonA.handleEvent(event):
-            print('User clicked button A.')
+            print('El usuario hizo clic en el botón A.')
         elif oButtonB.handleEvent(event):
-            print('User clicked button B.')
+            print('El usuario hizo clic en el botón B.')
         elif oButtonC.handleEvent(event):
-            print('User clicked button C.')
+            print('El usuario hizo clic en el botón C.')
 
-
-    # 8 - Do any "per frame" actions
+    # 8 - Realizar acciones "por fotograma"
     
-    # 9 - Clear the window
+    # 9 - Limpiar la ventana
     window.fill(GRAY)
     
-    # 10 - Draw all window elements
+    # 10 - Dibujar todos los elementos de la ventana
     oButtonA.draw()
     oButtonB.draw()
     oButtonC.draw()
 
-    # 11 - Update the window
+    # 11 - Actualizar la ventana
     pygame.display.update()
 
-    # 12 - Slow things down a bit
-    clock.tick(FRAMES_PER_SECOND)  # make pygame wait
+    # 12 - Reducir la velocidad del bucle
+    clock.tick(FRAMES_PER_SECOND)  # Hacer que pygame espere
